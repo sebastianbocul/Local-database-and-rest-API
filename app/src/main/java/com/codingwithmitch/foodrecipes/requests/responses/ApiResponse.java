90e9a49b26de9b2d0209817b7bb834b506a0a3d6
sebastianbocul <seba23495@gmail.com>
2020-09-15 00:36:09 +0200

    Created generic ApiResponce class

---
+++ app/src/main/java/com/codingwithmitch/foodrecipes/requests/responses/ApiResponse.java
@@ -0,0 +1,60 @@
+package com.codingwithmitch.foodrecipes.requests.responses;
+
+import java.io.IOException;
+
+import retrofit2.Response;
+
+public class ApiResponse<T> {
+    public ApiResponse<T> create(Throwable error) {
+        return new ApiErrorResponse<>(!error.getMessage().equals("") ? error.getMessage() : "Unknown error!");
+    }
+
+    public ApiResponse<T> create(Response<T> response){
+        if(response.isSuccessful()){
+            T body = response.body();
+            //204 code - empty
+            if(body==null|| response.code()==204){
+                return new ApiEmptyResponse<>();
+            }
+            else {
+                return new ApiSuccessResponse<>(body);
+            }
+        }else {
+            String errorMessage = "";
+            try {
+                errorMessage= response.errorBody().string();
+            }catch (IOException e){
+                e.printStackTrace();
+                errorMessage=response.message();
+            }
+            return new ApiErrorResponse<>(errorMessage);
+        }
+    }
+
+    public class ApiSuccessResponse<T> extends ApiResponse<T> {
+        private T body;
+
+        public ApiSuccessResponse(T body) {
+            this.body = body;
+        }
+
+        public T getBody() {
+            return body;
+        }
+    }
+
+    public class ApiErrorResponse<T> extends ApiResponse<T> {
+        private String errorMessage;
+
+        public ApiErrorResponse(String errorMessage) {
+            this.errorMessage = errorMessage;
+        }
+
+        public String getErrorMessage() {
+            return errorMessage;
+        }
+    }
+
+    public class ApiEmptyResponse<T> extends ApiResponse<T> {
+    }
+}
